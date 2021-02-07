bdc7a5a2465411373ff3a7ea8b410cd46eda3e48
ebrahim-sani <ibraheemsani035@gmail.com>
2021-02-07 03:06:54 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-upwork-test
+
+========= Books Gallery ==========
---
+++ upwork-test/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h3>Ready to get started</h3>
+    </div>
+  );
+}
+
+export default App;
